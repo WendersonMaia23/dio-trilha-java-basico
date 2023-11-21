28a921682c8b52a5a375188a91df351bfacdc229
Wenderson23 <wendersonmaia11@hotmail.com>
2023-11-21 09:11:26 -0400

    teste

--- tipo-variaveis/src/TipoVariaveis.java
+++ tipo-variaveis/src/TipoVariaveis.java
@@ -1,6 +1,10 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
-        
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
     }
 }
